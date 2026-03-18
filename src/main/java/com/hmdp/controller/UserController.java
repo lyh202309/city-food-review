0c8a3d89883c2cc8d2fb96cfbc9f4fcf47320098
LYH <666@qq.com>
2026-03-19 00:19:21 +0800

    拦截器拦截后把threadLocal里面的值删了 现在只存userDTO进session了，这也意味着threadLocal里面存的也是userDTO了

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -6,6 +6,7 @@ import com.hmdp.dto.Result;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -64,7 +65,7 @@ public class UserController {
 
     @GetMapping("/me")
     public Result me(){
-        return Result.fail("功能未完成");
+        return Result.ok(UserHolder.getUser());
     }
 
     @GetMapping("/info/{id}")
--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,7 +1,10 @@
 package com.hmdp.utils;
 
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import org.apache.ibatis.plugin.Interceptor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +16,7 @@ public class LoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //拦截器，逻辑为：能够从请求中的session中获得有有效的用户信息（不像前面的用jwt判断什么的）
         HttpSession session = request.getSession();
-        User user = (User) session.getAttribute("user");
+        UserDTO user = (UserDTO) session.getAttribute("user");
         //只有正确登录，session才有可能有用户信息
         if(user == null) {
             //！！！给点信息
@@ -21,6 +24,12 @@ public class LoginInterceptor implements HandlerInterceptor {
             return false;
         }
         //有用户信息那还说啥了，通过吧
+        //直接存线程里面，以后同一个会话内所有的请求的服务层以及控制层全都能拿到这个信息
+        UserHolder.saveUser(user);
         return true;
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        UserHolder.removeUser();
     }
 }

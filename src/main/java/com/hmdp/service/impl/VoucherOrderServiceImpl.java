5dd3ff46a330c11a27e8ef3de8c8a4d9d0fdbd2b
LYH <666@qq.com>
2026-03-22 23:40:35 +0800

    添加了秒杀抢券的逻辑，同时使用乐观锁和写入空值到redis来解决缓存击穿和缓存穿透问题。 添加了生成全局唯一id的工具类

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +24,49 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
-
+    @Autowired
+    SeckillVoucherServiceImpl seckillVoucherService;
+    @Autowired
+    RedisIdWorker redisIdWorker;
+    @Transactional
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        //查那个秒杀券是需要用那个秒杀券的服务层的mybatis-plus来查的，我们这个类有的是查秒杀券订单的
+        //1.先去查那个秒杀券
+        SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
+        //判断是否是属于有效时间
+        if(seckillVoucher.getBeginTime().isAfter(LocalDateTime.now())){
+            //还没开始
+            return Result.fail("秒杀还没开始");
+        }
+        if(seckillVoucher.getEndTime().isBefore(LocalDateTime.now())){
+            //已经结束
+            return Result.fail("秒杀已经结束");
+        }
+        //判断是否还有剩余
+        if(seckillVoucher.getStock() <= 0){
+            return Result.fail("券已售罄");
+        }
+        //还有剩余，更新吧，乐观锁
+        //要学这里的sql语句，还有就是mybatis-plus知识，还可以学到时间的api,还有.eq("voucher_id", voucherId)的意思
+        //关注这里的链式调用的返回值就看懂了
+        if(!seckillVoucherService.update().setSql("stock = stock - 1").eq("voucher_id",voucherId).gt("stock",0).update()){
+            //如果这条语句能够更新失败了，那就是没有库存了，这句话本身就是乐观锁了，不要怕
+            return Result.fail("券已售罄");
+        }
+        //更新成功了，那这个请求就成了，给他新建订单吧
+        //这里不担心先后顺序，新建订单这里没有线程安全问题，别人先建就先建呗
+        //涉及两个表的操作加个 事务？
+        VoucherOrder voucherOrder = new VoucherOrder();
+        long voucherOrderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(voucherOrderId);
+        voucherOrder.setVoucherId(voucherId);
+        //用户id设置在拦截器里面了，拦截器帮你放线程里面了，你这里直接取就行了
+        /*(只要登录，用户信息就会被放到缓存里面，缓存过期前多个请求之间互通，登录成功自动有一个请求，必然过全局拦截器
+        过全局拦截器更新你的token过期时间顺便把用户DTO存线程里面了，所以不需要来回传参。token对应redis里面的用户DTO对象)
+         */
+        voucherOrder.setUserId(UserHolder.getUser().getId());
+        this.save(voucherOrder);
+        return Result.ok(voucherOrderId);
+    }
 }
